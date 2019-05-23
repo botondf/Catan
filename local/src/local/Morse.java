fa97127be52a382ab0096c916784c9cba86d87f3
Botond Fasanga <botond@fasanga.com>
2019-05-23 09:31:17 -0400

    initial commit

---
+++ local/src/local/Morse.java
@@ -0,0 +1,21 @@
+package local;
+
+public class Morse {
+
+//	String code;
+//	String character;
+//	int trinary;
+	
+	Morse(String code, String character) {
+//		this.code = code;
+//		this.character = character;
+//		this.trinary = trinary;
+	}
+	
+	Morse() {};
+	
+	public void setChar() {
+		
+	}
+	
+}
---
+++ local/src/local/Test.java
@@ -0,0 +1,9 @@
+Timer timer = new Timer();
+TimerTask task = new TimerTask()
+{
+        public void run()
+        {
+            //The task you want to do       
+        }
+
+};
